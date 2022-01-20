03c72e57030e5b2aaf7967a209b33c889583e6a9
Diegudeveloper <diegudeveloper@gmail.com>
2022-01-20 11:55:39 +0100

    Configuracion del boton close del form

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 const searchBtn = document.querySelector(".fa-search");
 const searchContainer = document.querySelector(".search");
-const btn_menu = document.querySelector(".btn_menu")
-const navbar = document.querySelector(".navbar")
+const btn_menu = document.querySelector(".btn_menu");
+const navbar = document.querySelector(".navbar");
 
-const btnUser = document.querySelector(".fa-user")
+const btnUser = document.querySelector(".fa-user");
 
-const btnForm = document.querySelector(".container_login")
-const btnX = document.querySelector(".fa-times")
+const btnForm = document.querySelector(".container_login");
+const btnX = document.querySelector("#btn_close");
 
-btnUser.addEventListener("click", () => {
-    btnForm.classList.toggle('active');
+btnUser.addEventListener('click', () => {
+    btnForm.classList.add('active');
 });
+
+btnX.addEventListener('click', () => {
+    btnForm.classList.remove('active');
+});
+
 
 btn_menu.addEventListener("click", () => {
     navbar.classList.toggle('navbar_active');
